2d8a2dba7c09e04cc16a16b690e55500be0d788d
Oscar Navarrete <oscar.nvergara@gmail.com>
2025-07-27 19:02:55 -0400

    feature: terraform config y front end

--- infrastructure/modules/backend/server.tf
+++ infrastructure/modules/backend/server.tf
@@ -1,23 +1,31 @@
-resource "google_cloud_run_service" "backend" {
+
+
+resource "google_cloud_run_service" "backend_service" {
   name = var.name
   location = var.location
 
   template {
     spec {
       containers {
-        image = var.image
+        image = "us-central1-docker.pkg.dev/tallersoftware-467001/bakery-tallersoftware/backend:latest"
         ports {
-          container_port = 3000
+          container_port = 8080
         }
       }
     }
   }
 
-
-}
-   traffics {
+  traffic {
     percent         = 100
     latest_revision = true
   }
+}
 
+# 3. Habilita las APIs necesarias
+resource "google_project_service" "artifact_registry" {
+  service = "artifactregistry.googleapis.com"
+}
 
+resource "google_project_service" "cloud_run" {
+  service = "run.googleapis.com"
+}
---
+++ infrastructure/modules/frontend/website.tf
@@ -0,0 +1,9 @@
+resource "google_cloud_run_service" "website" {
+
+
+    name = var.websitename
+    location = var.location
+
+
+    
+}
