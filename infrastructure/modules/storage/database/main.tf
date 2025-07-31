6cf1629a8e4e0c1df0b6a613cc8fd819316ca1bf
Oscar Navarrete <oscar.nvergara@gmail.com>
2025-07-30 21:19:21 -0400

    network vpc

--- infrastructure/modules/storage/database/main.tf
+++ infrastructure/modules/storage/database/main.tf
@@ -20,6 +20,19 @@ resource "google_compute_network" "vpc" {
   name = "private-vpc"
   auto_create_subnetworks = false
 }
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
+}
+
+resource "google_compute_global_address" "private_ip_range" {
+  name          = "google-managed-services-my-vpc"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc.id
+}
 
 resource "google_compute_subnetwork" "subnet" {
   name          = "private-subnet"
