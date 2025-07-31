799238f81eb60c0e5d082bb684616ba5add5e876
Oscar Navarrete <oscar.nvergara@gmail.com>
2025-07-31 19:03:48 -0400

    corrected db

--- backend-server/src/database_connection/db.js
+++ backend-server/src/database_connection/db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "tallersoftware-467001:us-central1:postgres-instance",
+  database: "/cloudsql/tallersoftware-467001:us-central1:postgres-instance",
   host: process.env.DB_HOST, // Debe ser /cloudsql/...
   socketPath: process.env.DB_HOST, // Necesario para Cloud SQL
 });
