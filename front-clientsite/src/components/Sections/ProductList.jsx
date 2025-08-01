6d28993be6a222f6e59f4517a41439ce8bb2cdfc
Oscar Navarrete <oscar.nvergara@gmail.com>
2025-07-31 22:03:56 -0400

    error

--- front-clientsite/src/components/Sections/ProductList.jsx
+++ front-clientsite/src/components/Sections/ProductList.jsx
@@ -30,7 +30,7 @@ var varsita = useEffect(() => {
 
   return (
     <section className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {rows.map((producto) => (
+      {varsita.map((producto) => (
        <Product producto = {producto}/>
       ))
     }
