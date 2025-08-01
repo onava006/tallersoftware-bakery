c5a8a0de8fae695801774fff74f3a320addf3d88
Oscar Navarrete <oscar.nvergara@gmail.com>
2025-07-31 22:20:18 -0400

    solved issue request

--- front-clientsite/src/components/Sections/ProductList.jsx
+++ front-clientsite/src/components/Sections/ProductList.jsx
@@ -1,6 +1,6 @@
 // src/components/ProductList.jsx
 import Product from '../Elements/Product';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const productos = [
   { id: 1, nombre: "Pan Francés", precio: 1.2, imagen: "https://via.placeholder.com/150" },
@@ -11,18 +11,18 @@ const productos = [
   { id: 3, nombre: "Torta de Chocolate", precio: 15, imagen: "https://via.placeholder.com/150" },
 ];
 
-
-
-
 export default function ProductList() {
+
+const [products, setProducts] = useState();
 
 
 var varsita = useEffect(() => {
   fetch('https://backend-763457621296.us-central1.run.app/productos')
     .then(res => res.json())
+    .then(setProducts)
     .then(data => {
       console.log('Productos:', data);
-      return data.data.rows
+      
     })
     .catch(err => console.error('Error al obtener productos:', err));
 }, []);
@@ -30,11 +30,15 @@ var varsita = useEffect(() => {
 
   return (
     <section className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {varsita.map((producto) => (
+      {Array.isArray(varsita) ? 
+      
+      varsita.map((producto) => (
        <Product producto = {producto}/>
       ))
-    }
-
+    : (
+        <p>Cargando productos...</p>
+      )
+  }
     </section>
   );
 }
