b8580b7eb1b6856ce3e7efd34e3baea6e35eb18f
Oscar Navarrete <oscar.nvergara@gmail.com>
2025-07-31 23:12:30 -0400

    arregla dato front

--- front-clientsite/src/components/Elements/Product.jsx
+++ front-clientsite/src/components/Elements/Product.jsx
@@ -6,7 +6,8 @@ export default function Product({producto}) {
 <div key={producto.id} className="border rounded-lg p-4 shadow hover:shadow-lg">
           <img src={producto.imagen} alt={producto.nombre} className="w-full h-40 object-cover rounded" />
           <h2 className="text-xl font-semibold mt-2">{producto.nombre}</h2>
-          <p className="text-gray-700">${producto.precio.toFixed(2)}</p>
+          <p className="text-gray-700">${producto.precio}</p>
+           <p className="text-gray-700">${producto.descripcion}</p>
         </div>
         </Button> 
        ); 
